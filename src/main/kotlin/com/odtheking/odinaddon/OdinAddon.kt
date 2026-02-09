910aa575e8205e4077ae367abbd0f17f5e3015cc
kiwi <85279289+mizu-kiwi@users.noreply.github.com>
2026-02-09 12:23:08 +0900

    1.8.9 never ends

---
+++ src/main/kotlin/com/odtheking/odinaddon/OdinAddon.kt
@@ -0,0 +1,22 @@
+package com.odtheking.odinaddon
+
+import com.odtheking.odin.config.ModuleConfig
+import com.odtheking.odin.events.core.EventBus
+import com.odtheking.odin.features.ModuleManager
+import com.odtheking.odinaddon.features.impl.floor7.MelodyMessage
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
+
+object OdinAddon : ClientModInitializer {
+
+    override fun onInitializeClient() {
+        println("Low Guy initialized!")
+
+
+        // Register objects to event bus by adding to the list
+        listOf(this).forEach { EventBus.subscribe(it) }
+
+        // Register modules by adding to the list
+        ModuleManager.registerModules(ModuleConfig("OdinAddon.json"), MelodyMessage)
+    }
+}
